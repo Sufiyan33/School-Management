c9a54f157545ab28035197644b22d6c366d01bb9
Sufiyan33 <basu700736@gmail.com>
2024-11-19 12:41:30 +0530

    Created security configuration

---
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.sufiyan.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+	@Bean
+	public PasswordEncoder passwordEnconder() {
+		return new BCryptPasswordEncoder();
+	}
+}
