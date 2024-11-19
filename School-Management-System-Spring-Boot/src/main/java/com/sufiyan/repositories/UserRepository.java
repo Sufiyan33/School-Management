d78825309f24da3f085c908e85fd4f12c651fe00
Sufiyan33 <basu700736@gmail.com>
2024-11-19 12:39:13 +0530

    Created repository for user

---
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sufiyan.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sufiyan.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
