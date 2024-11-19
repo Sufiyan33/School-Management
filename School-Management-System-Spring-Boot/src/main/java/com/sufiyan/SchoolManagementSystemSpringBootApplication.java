bb9fc94b315422dd4791e7bc962e1680432a0e60
Sufiyan33 <basu700736@gmail.com>
2024-11-19 12:00:00 +0530

    Created backend application for school management

---
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/SchoolManagementSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sufiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemSpringBootApplication.class, args);
+	}
+
+}
