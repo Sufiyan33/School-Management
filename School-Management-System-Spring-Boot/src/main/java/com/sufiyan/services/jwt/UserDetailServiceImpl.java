8dc245298281e8181496d312a75063ff25696636
Sufiyan33 <basu700736@gmail.com>
2024-11-20 23:17:54 +0530

    Fetching user using email

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/jwt/UserDetailServiceImpl.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/jwt/UserDetailServiceImpl.java
@@ -1,5 +1,33 @@
 package com.sufiyan.services.jwt;
 
-public class UserDetailServiceImpl {
+import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sufiyan.entities.User;
+import com.sufiyan.repositories.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	// Write logic to get user from DB.
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> optionalUser = userRepo.findFirstByEmail(email);
+		if(optionalUser.isEmpty())
+			throw new UsernameNotFoundException("username not found", null);
+		
+		return new org.springframework.security.core.userdetails.User(
+					optionalUser.get().getEmail(), 
+					optionalUser.get().getPassword(), 
+					new ArrayList<>());
+	}
 }
