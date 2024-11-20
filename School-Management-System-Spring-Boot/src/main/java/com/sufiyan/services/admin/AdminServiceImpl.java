150c449ea6228d1e8a00841af3157330235e2042
Sufiyan33 <basu700736@gmail.com>
2024-11-20 10:58:56 +0530

    Adding postConstruct to create admin

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/admin/AdminServiceImpl.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/admin/AdminServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.sufiyan.entities.User;
 import com.sufiyan.repositories.UserRepository;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class AdminServiceImpl {
 	
@@ -15,6 +17,10 @@ public class AdminServiceImpl {
 		this.userRepo = userRepo;
 	}
 	
+	/*
+	 We don't need call by using api, when app start then it will run automatically
+	 */
+	@PostConstruct
 	public void createAdminAccount() {
 		User adminAccount = new User();
 		adminAccount.setEmail("admin@test.com");
