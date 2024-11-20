c6e63e40f656b4133a9e3765b45d9515c0bdb4f7
Sufiyan33 <basu700736@gmail.com>
2024-11-20 23:13:04 +0530

    Fetching user by email

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sufiyan.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import com.sufiyan.enums.UserRole;
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	User findByRole(UserRole role);
+
+	Optional<User> findFirstByEmail(String email);
 }
