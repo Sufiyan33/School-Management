718e0d9cc5bb0ad10746fc9abf5ece8d4747cd8e
Sufiyan33 <basu700736@gmail.com>
2024-11-20 11:08:39 +0530

    Fetching user by admin Role

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sufiyan.entities.User;
+import com.sufiyan.enums.UserRole;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	User findByRole(UserRole role);
 }
