c013307e1ff438daafb9b07004712986b2f24d27
Sufiyan33 <basu700736@gmail.com>
2024-11-21 13:11:51 +0530

    Creating Authentication Response dto

---
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/dto/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.sufiyan.dto;
+
+public class AuthenticationResponse {
+
+	private String jwtToken;
+	
+	public AuthenticationResponse(String jwt) {}
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+}
