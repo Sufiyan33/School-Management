987d8564278b5036c305f150fe0941c89f96ce68
Sufiyan33 <basu700736@gmail.com>
2024-11-21 15:08:47 +0530

    authorising request

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/configurations/WebSecurityConfiguration.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/configurations/WebSecurityConfiguration.java
@@ -4,12 +4,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
+	// Let's authorize our endpoinds
+	@Bean
+	public SecurityFilterChain sucSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/authenticate").permitAll()
+				.anyRequest().authenticated())
+				.sessionManagement(
+						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		return http.build();
+				
+				
+		/*return http.csrf().disable()
+		.authorizeHttpRequests()
+		.requestMatchers("/authenticate")
+		.permitAll()
+		.and()
+		.authorizeHttpRequests()
+		.requestMatchers("")
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.build();*/
+	} 
 	@Bean
 	public PasswordEncoder passwordEnconder() {
 		return new BCryptPasswordEncoder();
