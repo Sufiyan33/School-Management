a0d02821c48836d6cab1b556e1f3db0caf760d87
Sufiyan33 <basu700736@gmail.com>
2024-11-22 16:57:15 +0530

    Added styling and import

--- School-Management-System-Angular/src/app/components/login/login.component.ts
+++ School-Management-System-Angular/src/app/components/login/login.component.ts
@@ -3,11 +3,12 @@ import { AuthService } from '../../services/auth/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, ReactiveFormsModule, MatInput],
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatInput,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
