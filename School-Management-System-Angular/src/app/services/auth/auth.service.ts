f2e3466ea75f495b75f2517c1d5608f15438c18a
Sufiyan33 <basu700736@gmail.com>
2024-11-23 09:57:33 +0530

    Setting headers and saving in local storage

--- School-Management-System-Angular/src/app/services/auth/auth.service.ts
+++ School-Management-System-Angular/src/app/services/auth/auth.service.ts
@@ -1,17 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Constant } from '../../const/Constants';
+import { StorageService } from '../storage/storage.service';
 
-//const BASIC_URL = ['http://localhost:8080/'];
+export const AUTH_HEADER = 'authorization';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storage: StorageService) { }
 
-  login(loginRequest: any): Observable<any>{
-    return this.http.post(Constant.BASIC_URL + "authenticate", loginRequest);
+  login(email: string, password: string): Observable<any>{
+    return this.http.post(Constant.BASIC_URL + "authenticate", {
+      email,
+      password
+    },{observe: 'response'})
+    .pipe(
+      tap(__ => this.log("User Authentication")),
+      map((res: HttpResponse<any>) =>{
+        this.storage.saveUser(res.body);
+        const tokenLength = res.headers.get(AUTH_HEADER).length;
+        const bearerToken = res.headers.get(AUTH_HEADER).substring(7, tokenLength);
+        this.storage.saveToken(bearerToken);
+        return res;
+      })
+    )
+  }
+
+  log(message: string){
+    console.log(message);
   }
 }
