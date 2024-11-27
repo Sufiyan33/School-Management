d8715e07860b77c4ad0ce2d169b6bc4f3d46b040
Sufiyan33 <basu700736@gmail.com>
2024-11-27 09:14:54 +0530

    removed unused import

--- School-Management-System-Angular/src/app/auth/guards/noAuth-guard/no-auth.guard.ts
+++ School-Management-System-Angular/src/app/auth/guards/noAuth-guard/no-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { StorageService } from '../../../services/storage/storage.service';
 
 @Injectable({
