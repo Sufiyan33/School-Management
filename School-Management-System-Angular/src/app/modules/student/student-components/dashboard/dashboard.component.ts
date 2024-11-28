c402ee438adcdcab293ad265b78640b7b157d75f
Sufiyan33 <basu700736@gmail.com>
2024-11-28 08:41:00 +0530

    Added host to make difference

--- School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
+  host: {'data-id': 'dashboard-instance1'},
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
