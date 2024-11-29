1fb2cffd6b8cf8f37e8c1c198504db0517416efe
Sufiyan33 <basu700736@gmail.com>
2024-11-29 22:53:09 +0530

    Injecting adminService in controller

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
@@ -3,8 +3,16 @@ package com.sufiyan.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sufiyan.services.admin.AdminService;
+
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
+	private final AdminService adminService;
+
+	public AdminController(AdminService adminService) {
+		super();
+		this.adminService = adminService;
+	}
 }
