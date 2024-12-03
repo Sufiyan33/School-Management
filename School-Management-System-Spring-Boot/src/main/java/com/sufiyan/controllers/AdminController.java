4704d386beebce6c3c1bca58ba95c6d8012561fb
Sufiyan33 <basu700736@gmail.com>
2024-12-03 23:22:17 +0530

    Post Student method

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
@@ -1,8 +1,11 @@
 package com.sufiyan.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sufiyan.dto.StudentDto;
 import com.sufiyan.services.admin.AdminService;
 
 @RestController
@@ -14,5 +17,9 @@ public class AdminController {
 	public AdminController(AdminService adminService) {
 		super();
 		this.adminService = adminService;
+	}
+	
+	public ResponseEntity<?> addStendent(@RequestBody StudentDto studentDto){
+		StudentDto createdStudent = adminService.posStudent(studentDto);
 	}
 }
