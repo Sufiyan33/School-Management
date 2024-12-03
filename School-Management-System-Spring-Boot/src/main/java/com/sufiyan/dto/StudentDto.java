14bb241a8c5c2fcfc3a81c26649c172fea2685de
Sufiyan33 <basu700736@gmail.com>
2024-12-03 23:17:21 +0530

    Created DTO class with fileds

---
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/dto/StudentDto.java
@@ -0,0 +1,18 @@
+package com.sufiyan.dto;
+
+import java.util.Date;
+
+public class StudentDto {
+
+	private String name;
+	private String email;
+	private String password;
+	private String fatherName;
+	private String motherName;
+	private String StudentClass;
+	private Date dob;
+	private String address;
+	private String gender;
+	
+	
+}
