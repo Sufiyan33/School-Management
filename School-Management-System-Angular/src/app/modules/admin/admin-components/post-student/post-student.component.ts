a5931ee2303515b1c866a39bc5abd5301358a43f
Sufiyan33 <basu700736@gmail.com>
2024-12-05 19:19:38 +0530

    Added common module as of now later will change this

--- School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDatepicker, MatDatepickerModule, MatDatepickerToggle } from '@angular/material/datepicker';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
@@ -8,10 +9,14 @@ import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select'
   selector: 'app-post-student',
   standalone: true,
   imports: [MatProgressSpinnerModule, MatFormFieldModule, MatLabel, MatSelectModule, 
-    MatOption, MatDatepickerModule, MatDatepickerToggle],
+    MatOption, MatDatepickerModule, MatDatepickerToggle, CommonModule],
   templateUrl: './post-student.component.html',
   styleUrl: './post-student.component.css'
 })
 export class PostStudentComponent {
+
+  CLASS: string[]= ["play", "1st", "2nd","3rd","4th","5th", "6th", "7th","8th", "9th", "10th", "11th", "12th"];
+  GENDER: string[]= ["Male", "Female", "Not specified"];
+  isSpinning: boolean;
 
 }
