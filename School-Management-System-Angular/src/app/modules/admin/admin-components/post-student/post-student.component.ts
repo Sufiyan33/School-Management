0df4e2b1afb1d285bd119806b50cf8028d9dfe7e
Sufiyan33 <basu700736@gmail.com>
2024-12-05 23:15:49 +0530

    Created form group

--- School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepicker, MatDatepickerModule, MatDatepickerToggle } from '@angular/material/datepicker';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select';
+import { AdminService } from '../../admin-service/admin.service';
 
 @Component({
   selector: 'app-post-student',
@@ -14,10 +15,31 @@ import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select'
   templateUrl: './post-student.component.html',
   styleUrl: './post-student.component.css'
 })
-export class PostStudentComponent {
+export class PostStudentComponent implements OnInit{
 
   CLASS: string[]= ["play", "1st", "2nd","3rd","4th","5th", "6th", "7th","8th", "9th", "10th", "11th", "12th"];
   GENDER: string[]= ["Male", "Female", "Not specified"];
   isSpinning: boolean;
+  validateForm: FormGroup;
+
+  constructor(
+    private service: AdminService,
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: ['null', Validators.required],
+      name: ['null', Validators.required],
+      password: ['null', Validators.required],
+      checkPassword: ['null', Validators.required],
+      fatherName: ['null', Validators.required],
+      motherName: ['null', Validators.required],
+      studentClass: ['null', Validators.required],
+      dateOfBirth: ['null', Validators.required],
+      address: ['null', Validators.required],
+      gender: ['null', Validators.required]
+    })
+  }
 
 }
