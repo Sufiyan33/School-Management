9ea18b329ac4967e63f779fe34e5e0c584342467
Sufiyan33 <basu700736@gmail.com>
2024-12-05 18:02:28 +0530

    Created post-student component

---
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-student.component.html',
+  styleUrl: './post-student.component.css'
+})
+export class PostStudentComponent {
+
+}
