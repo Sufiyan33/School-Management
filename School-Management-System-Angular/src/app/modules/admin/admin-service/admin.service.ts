43452743291f11a8849acdb4928bb77089df1c1e
Sufiyan33 <basu700736@gmail.com>
2024-12-05 18:15:01 +0530

    Calling post api from backend

--- School-Management-System-Angular/src/app/modules/admin/admin-service/admin.service.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-service/admin.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Constant } from '../../../const/Constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addStudent(studentDto: any){
+    this.http.post<[]>(Constant.BASIC_URL + "api/admin/student", studentDto);
+  }
 }
