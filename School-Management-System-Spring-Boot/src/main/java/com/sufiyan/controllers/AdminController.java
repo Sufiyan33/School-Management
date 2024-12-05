9ea40518877c32cdbb621e0b1fdc557f7e42a0fd
Sufiyan33 <basu700736@gmail.com>
2024-12-05 17:54:23 +0530

    end point for post student

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
@@ -1,6 +1,8 @@
 package com.sufiyan.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,11 @@ public class AdminController {
 		this.adminService = adminService;
 	}
 	
+	@PostMapping("/student")
 	public ResponseEntity<?> addStendent(@RequestBody StudentDto studentDto){
 		StudentDto createdStudent = adminService.posStudent(studentDto);
+		if(createdStudent == null)
+			return new ResponseEntity<>("something wrong.", HttpStatus.BAD_REQUEST);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
 	}
 }
