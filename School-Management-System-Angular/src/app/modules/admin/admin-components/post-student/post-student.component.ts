8693b0ec068abde75a5191ecc83895293a9546fc
Sufiyan33 <basu700736@gmail.com>
2024-12-05 18:54:25 +0530

    Added import

--- School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/post-student/post-student.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDatepicker, MatDatepickerToggle } from '@angular/material/datepicker';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSpinner } from '@angular/material/progress-spinner';
 import { MatOption, MatSelect } from '@angular/material/select';
@@ -6,7 +7,7 @@ import { MatOption, MatSelect } from '@angular/material/select';
 @Component({
   selector: 'app-post-student',
   standalone: true,
-  imports: [MatSpinner, MatFormField, MatLabel, MatSelect, MatOption],
+  imports: [MatSpinner, MatFormField, MatLabel, MatSelect, MatOption, MatDatepicker, MatDatepickerToggle],
   templateUrl: './post-student.component.html',
   styleUrl: './post-student.component.css'
 })
