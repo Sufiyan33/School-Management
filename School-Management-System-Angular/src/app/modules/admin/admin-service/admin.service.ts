ac69d0d9ef954de3b4cd15476b0bf75220f43730
Sufiyan33 <basu700736@gmail.com>
2024-12-06 08:51:43 +0530

    Added token for authentication backend call

--- School-Management-System-Angular/src/app/modules/admin/admin-service/admin.service.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-service/admin.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Constant } from '../../../const/Constants';
+import { StorageService } from '../../../services/storage/storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,16 @@ export class AdminService {
   constructor(private http: HttpClient) { }
 
   addStudent(studentDto: any){
-    return this.http.post<[]>(Constant.BASIC_URL + "api/admin/student", studentDto);
+    return this.http.post<[]>(Constant.BASIC_URL + "api/admin/student", studentDto, {
+      headers: this.createAuthorizationHeader(),
+    });
   }
+
+  createAuthorizationHeader(): HttpHeaders{
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization', "Bearer " + StorageService.getToken()
+    );
+  }
+
 }
