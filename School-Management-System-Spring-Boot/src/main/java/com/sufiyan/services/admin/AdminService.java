3e59a50fdeea1d1dac88a37f95cd6a262aa72d32
Sufiyan33 <basu700736@gmail.com>
2024-12-08 12:13:51 +0530

    Added method to fetch all details

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/admin/AdminService.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/admin/AdminService.java
@@ -1,9 +1,12 @@
 package com.sufiyan.services.admin;
 
+import java.util.List;
+
 import com.sufiyan.dto.StudentDto;
 
 public interface AdminService {
 
 	StudentDto posStudent(StudentDto studentDto);
+	List<StudentDto> getAllStudents();
 
 }
