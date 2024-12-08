593387d57612a160f83bce74f01d5e8741e0c739
Sufiyan33 <basu700736@gmail.com>
2024-12-08 12:13:07 +0530

    Query to fetch data by using role

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.sufiyan.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	User findByRole(UserRole role);
 
 	Optional<User> findFirstByEmail(String email);
+	
+	List<User> findAllByRole(UserRole userRole);
 }
