f1e53b26f8264a8bf0a78c562dd3a0e89189ea1d
Sufiyan33 <basu700736@gmail.com>
2024-12-17 06:58:38 +0530

    Added delete end-points for admin

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/AdminController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +43,11 @@ public class AdminController {
 	public ResponseEntity<List<StudentDto>> getAllStudents(){
 		List<StudentDto> allStudents = adminService.getAllStudents();
 		return ResponseEntity.ok(allStudents);
+	}
+	
+	@DeleteMapping("/student/{studetnId}")
+	public ResponseEntity<Void> deleteStudent(@PathVariable Long studentId) {
+		adminService.deleteStudent(studentId);
+		return ResponseEntity.noContent().build();
 	}
 }
