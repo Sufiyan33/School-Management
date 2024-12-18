13b48df42f0fcd1f5a1c8e849e992d8f573803a4
Sufiyan33 <basu700736@gmail.com>
2024-12-18 14:11:41 +0530

    Added form validator

--- School-Management-System-Angular/src/app/modules/admin/admin-components/update-student/update-student.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/update-student/update-student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../admin-service/admin.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-student',
@@ -12,11 +13,25 @@ export class UpdateStudentComponent implements OnInit{
   
   constructor(
     private service: AdminService,
-    private activatedRouted: ActivatedRoute){}
+    private activatedRouted: ActivatedRoute,
+    private fb: FormBuilder){}
 
   studentId!:number;
+  validateForm: FormGroup;
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: ['', Validators.required],
+      name: ['', Validators.required],
+      password: ['', Validators.required],
+      checkPassword: ['', [Validators.required, this.confirmationValidator]],
+      fatherName: ['', Validators.required],
+      motherName: ['', Validators.required],
+      studentClass: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      address: ['', Validators.required],
+      gender: ['', Validators.required]
+    })
    this.studentId = this.activatedRouted.snapshot.params['studentId']
    this.getStudentById();
   }
