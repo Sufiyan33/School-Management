e4dc597f9d1f9f2b4a7a7662353de6a1b72b2689
Sufiyan33 <basu700736@gmail.com>
2024-12-20 09:40:43 +0530

    Added import

--- School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatOption } from '@angular/material/core';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-pay-fee',
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatFormFieldModule, MatLabel, MatSelectModule, 
+      MatOption],
   templateUrl: './pay-fee.component.html',
   styleUrl: './pay-fee.component.css'
 })
