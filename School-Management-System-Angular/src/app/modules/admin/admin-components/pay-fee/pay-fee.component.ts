a65efb2870129c616a12d48aa4081ec3947dc415
Sufiyan33 <basu700736@gmail.com>
2024-12-20 10:01:55 +0530

    Added form group and builder

--- School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatOption } from '@angular/material/core';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { AdminService } from '../../admin-service/admin.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pay-fee',
+  standalone: true,
   imports: [MatProgressSpinnerModule, MatFormFieldModule, MatLabel, MatSelectModule, 
-      MatOption],
+      MatOption, CommonModule, ReactiveFormsModule],
   templateUrl: './pay-fee.component.html',
   styleUrl: './pay-fee.component.css'
 })
@@ -21,17 +23,23 @@ export class PayFeeComponent implements OnInit{
     private fb: FormBuilder
   ){}
 
-  ngOnInit(): void {
-    this.studentId = this.activatedRouted.snapshot.params['studentId']
-  }
-
   studentId!:number;
   validateForm: FormGroup;
   isSpinning: boolean = false;
   MONTH: string[] = ["January", "February", "March", "April", "May", "June", 
     "July", "August", "September", "October", "November", "December"];
 
-  payFee(){
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      amount: ['', Validators.required],
+      month: ['', Validators.required],
+      givenBy: ['', Validators.required],
+      description: ['', Validators.required],
+    })
+    this.studentId = this.activatedRouted.snapshot.params['studentId']
+  }
 
+  payFee(){
+    console.log(this.validateForm.value)
   }
 }
