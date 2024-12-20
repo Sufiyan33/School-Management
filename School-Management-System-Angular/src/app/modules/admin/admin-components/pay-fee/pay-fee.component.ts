c0f77bcf542fff3fc603f8dca8e4e2e7fde0214b
Sufiyan33 <basu700736@gmail.com>
2024-12-20 09:50:38 +0530

    Added constructor and defining fields

--- School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatOption } from '@angular/material/core';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { AdminService } from '../../admin-service/admin.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pay-fee',
@@ -13,4 +16,18 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class PayFeeComponent {
 
+  constructor(private service: AdminService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ){}
+  
+  studentId!:number;
+  validateForm: FormGroup;
+  isSpinning: boolean = false;
+  MONTH: string[] = ["January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"];
+
+  payFee(){
+
+  }
 }
