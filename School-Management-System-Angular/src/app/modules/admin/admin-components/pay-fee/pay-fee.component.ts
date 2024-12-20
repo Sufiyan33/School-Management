07fdcb977d7444785ff405a1811e27bbd904072c
Sufiyan33 <basu700736@gmail.com>
2024-12-20 08:48:57 +0530

    created pay-fee component

---
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/pay-fee/pay-fee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-fee',
+  imports: [],
+  templateUrl: './pay-fee.component.html',
+  styleUrl: './pay-fee.component.css'
+})
+export class PayFeeComponent {
+
+}
