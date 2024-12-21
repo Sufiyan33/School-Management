47e957597a8a7f4eb67342a8659e82d45c2eb2db
Sufiyan33 <basu700736@gmail.com>
2024-12-21 07:54:42 +0530

    Making api call and displaying data on ui

--- School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../student-service/student.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit{
 
-  constructor(){}
+  constructor(private service: StudentService){}
 
   ngOnInit(): void {
-    
+    this.getStudentById();
+  }
+
+  getStudentById(){
+    this.service.getStudentById().subscribe((res)=>{
+      console.log(res);
+    })
   }
 
 }
