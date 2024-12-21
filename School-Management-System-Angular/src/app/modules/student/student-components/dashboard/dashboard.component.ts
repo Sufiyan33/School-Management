eb23d3e8efae39077445f57cbdea86f8bead87dc
Sufiyan33 <basu700736@gmail.com>
2024-12-21 08:07:52 +0530

    Added import which are required

--- School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../student-service/student.service';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   host: {'data-id': 'dashboard-instance1'},
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule, MatButtonModule, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit{
 
+  student: any;
   constructor(private service: StudentService){}
 
   ngOnInit(): void {
@@ -20,6 +25,7 @@ export class DashboardComponent implements OnInit{
   getStudentById(){
     this.service.getStudentById().subscribe((res)=>{
       console.log(res);
+      this.student = res.student;
     })
   }
 
