16eba10ea9fe9c62d03c70da62656ab463a705a5
Sufiyan33 <basu700736@gmail.com>
2024-12-21 07:38:30 +0530

    Making call to backend

--- School-Management-System-Angular/src/app/modules/student/student-service/student.service.ts
+++ School-Management-System-Angular/src/app/modules/student/student-service/student.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStudentById(studentId: number): Observable<any>{
+      return this.http.get<[]>(Constant.BASIC_URL + `api/admin/student/${studentId}`, {
+        headers: this.createAuthorizationHeader()
+      }) 
+    }
 }
