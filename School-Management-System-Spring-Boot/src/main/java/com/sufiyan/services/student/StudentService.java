df7cb68fc47b13d1ffe82ee3a1402b48a2cdabd8
Sufiyan33 <basu700736@gmail.com>
2024-12-21 07:19:56 +0530

    Implementing method to get student by id

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentService.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentService.java
@@ -1,5 +1,9 @@
 package com.sufiyan.services.student;
 
+import com.sufiyan.dto.SingleStudentDto;
+
 public interface StudentService {
+
+	SingleStudentDto getStudentById(Long id);
 
 }
--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentServiceImpl.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentServiceImpl.java
@@ -1,15 +1,27 @@
 package com.sufiyan.services.student;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.sufiyan.dto.SingleStudentDto;
+import com.sufiyan.entities.User;
 import com.sufiyan.repositories.UserRepository;
 
 @Service
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService{
 
 	private final UserRepository userRepository;
 	
 	public StudentServiceImpl(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+
+	@Override
+	public SingleStudentDto getStudentById(Long id) {
+		Optional<User> existingUser = userRepository.findById(id);
+		SingleStudentDto singleStudentDto = new SingleStudentDto();
+		existingUser.ifPresent(user -> singleStudentDto.setStudentDto(user.getStudentDto()));
+		return singleStudentDto;
 	}
 }
