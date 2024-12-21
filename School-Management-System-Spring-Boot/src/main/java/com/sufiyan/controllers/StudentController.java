d84c72301f76c2770add0e72782e8c60fd8e48e6
Sufiyan33 <basu700736@gmail.com>
2024-12-21 07:19:35 +0530

    Creating new end-point for studentGetById

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/StudentController.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/controllers/StudentController.java
@@ -1,8 +1,12 @@
 package com.sufiyan.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sufiyan.dto.SingleStudentDto;
 import com.sufiyan.services.student.StudentService;
 
 @RestController
@@ -13,5 +17,13 @@ public class StudentController {
 	
 	public StudentController(StudentService studentService) {
 		this.studentService = studentService;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<SingleStudentDto> getStudentById(@PathVariable Long id){
+		SingleStudentDto singleStudentDto = studentService.getStudentById(id);
+		if(singleStudentDto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(singleStudentDto);
 	}
 }
