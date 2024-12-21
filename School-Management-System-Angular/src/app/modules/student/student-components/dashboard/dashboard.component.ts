909b3c378392c2fb15b11ce44e28a65ed54ac6c3
Sufiyan33 <basu700736@gmail.com>
2024-12-21 07:35:41 +0530

    Created constructor and onInit

--- School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
