4a463d757cb4a43a4aee98555df00c39caa2e21e
Sufiyan33 <basu700736@gmail.com>
2024-12-22 07:47:59 +0530

    Added validatorForm

--- School-Management-System-Angular/src/app/modules/student/student-components/apply-leave/apply-leave.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/apply-leave/apply-leave.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { StudentService } from '../../student-service/student.service';
 
 @Component({
   selector: 'app-apply-leave',
@@ -13,6 +14,21 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   templateUrl: './apply-leave.component.html',
   styleUrl: './apply-leave.component.css'
 })
-export class ApplyLeaveComponent {
+export class ApplyLeaveComponent implements OnInit{
+
+  isSpinning: boolean = false;
+  validateForm: FormGroup;
+
+  constructor(private service: StudentService,
+    private fb: FormBuilder,
+
+  ){}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      subject: ['', Validators.required],
+      body: ['', Validators.required]
+    });
+  }
 
 }
