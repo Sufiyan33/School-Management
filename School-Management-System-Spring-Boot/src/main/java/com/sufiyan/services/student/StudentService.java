9929a2e858aa0b2fbe791bfcc51cf11cfdf4235c
Sufiyan33 <basu700736@gmail.com>
2024-12-22 08:59:47 +0530

    Implemented method to fetch record with leave

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentService.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/services/student/StudentService.java
@@ -1,5 +1,7 @@
 package com.sufiyan.services.student;
 
+import java.util.List;
+
 import com.sufiyan.dto.SingleStudentDto;
 import com.sufiyan.dto.StudentLeaveDto;
 
@@ -8,5 +10,7 @@ public interface StudentService {
 	SingleStudentDto getStudentById(Long id);
 
 	StudentLeaveDto applyLeave(StudentLeaveDto studentLeaveDto);
+
+	List<StudentLeaveDto> getAllAppliedLeaveByStudent(Long userId);
 
 }
