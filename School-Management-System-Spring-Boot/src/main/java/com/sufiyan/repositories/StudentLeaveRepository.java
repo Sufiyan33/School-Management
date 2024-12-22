287417e56d24af49dd35c6e05f5c149bb9a2aee5
Sufiyan33 <basu700736@gmail.com>
2024-12-22 08:59:22 +0530

    Implemented new method to fetch list of data

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/StudentLeaveRepository.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/StudentLeaveRepository.java
@@ -1,11 +1,16 @@
 package com.sufiyan.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.sufiyan.dto.StudentLeaveDto;
 import com.sufiyan.entities.StudentLeave;
 
 @Repository
 public interface StudentLeaveRepository extends JpaRepository<StudentLeave, Long> {
+
+	List<StudentLeaveDto> findAllByUserId(Long userId);
 
 }
