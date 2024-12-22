729a25696652cb51e82027997f46e1d119818475
Sufiyan33 <basu700736@gmail.com>
2024-12-22 07:42:47 +0530

    Added import

--- School-Management-System-Angular/src/app/modules/student/student-components/apply-leave/apply-leave.component.ts
+++ School-Management-System-Angular/src/app/modules/student/student-components/apply-leave/apply-leave.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-apply-leave',
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatFormFieldModule, MatLabel, 
+          CommonModule, ReactiveFormsModule, FormsModule, MatInputModule, MatButtonModule],
   templateUrl: './apply-leave.component.html',
   styleUrl: './apply-leave.component.css'
 })
