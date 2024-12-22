3c67bf698565712ed1ed3d8709aadeb5d82da15d
Sufiyan33 <basu700736@gmail.com>
2024-12-22 07:33:53 +0530

    Making call to backend

--- School-Management-System-Angular/src/app/modules/student/student-service/student.service.ts
+++ School-Management-System-Angular/src/app/modules/student/student-service/student.service.ts
@@ -17,6 +17,12 @@ export class StudentService {
     }) 
   }
 
+  applyLeave(studentLeaveDto: any): Observable<any>{
+    return this.http.post<[]>(Constant.BASIC_URL + `api/student/leave`, studentLeaveDto, {
+      headers: this.createAuthorizationHeader()
+  }) 
+}
+
   createAuthorizationHeader(): HttpHeaders{
       let authHeaders: HttpHeaders = new HttpHeaders();
       let token = StorageService.getToken();
