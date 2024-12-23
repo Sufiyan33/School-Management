931418bb86135dc47bf8fbe0ccd8934924b7dc15
Sufiyan33 <basu700736@gmail.com>
2024-12-23 07:45:26 +0530

    Changing return type of query

--- School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/StudentLeaveRepository.java
+++ School-Management-System-Spring-Boot/src/main/java/com/sufiyan/repositories/StudentLeaveRepository.java
@@ -5,12 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sufiyan.dto.StudentLeaveDto;
 import com.sufiyan.entities.StudentLeave;
 
 @Repository
 public interface StudentLeaveRepository extends JpaRepository<StudentLeave, Long> {
 
-	List<StudentLeaveDto> findAllByUserId(Long userId);
+	List<StudentLeave> findAllByUserId(Long userId);
 
 }
