af3ab4896ef82c9e7837a2df8920fbdf999677c1
Sufiyan33 <basu700736@gmail.com>
2024-12-23 08:04:55 +0530

    Created new component to fetch all leave

---
+++ School-Management-System-Angular/src/app/modules/student/student-components/get-all-leave/get-all-leave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-leave',
+  imports: [],
+  templateUrl: './get-all-leave.component.html',
+  styleUrl: './get-all-leave.component.css'
+})
+export class GetAllLeaveComponent {
+
+}
