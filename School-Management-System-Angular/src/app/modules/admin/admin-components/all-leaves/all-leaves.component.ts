dfdcd10c537ce82624ab8fd95ab35a681b94d393
Sufiyan33 <basu700736@gmail.com>
2024-12-28 11:35:55 +0530

    Created new component for all leaves

---
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/all-leaves/all-leaves.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-leaves',
+  imports: [],
+  templateUrl: './all-leaves.component.html',
+  styleUrl: './all-leaves.component.css'
+})
+export class AllLeavesComponent {
+
+}
