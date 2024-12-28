4c6f3e3f395d3cf73360fcae4e301752a9e9914e
Sufiyan33 <basu700736@gmail.com>
2024-12-28 11:45:23 +0530

    Making api call

--- School-Management-System-Angular/src/app/modules/admin/admin-components/all-leaves/all-leaves.component.ts
+++ School-Management-System-Angular/src/app/modules/admin/admin-components/all-leaves/all-leaves.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-service/admin.service';
 
 @Component({
   selector: 'app-all-leaves',
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AllLeavesComponent {
 
+  isSpinning: boolean = false;
+  leaves: any;
+    
+  constructor(private service: AdminService,
+  ){}
+  
+  ngOnInit(): void {
+    this.getAllLeaves()
+  }
+  
+  getAllLeaves(){
+    this.isSpinning = true;
+    this.service.getAllAppliedLeaves().subscribe((res) =>{
+      console.log(res);
+      this.isSpinning = false;
+      this.leaves = res;
+    })
+  }
 }
